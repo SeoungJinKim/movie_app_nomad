fde4634ff0418ef2b8f30c4a1da0ad37d8effb19
SeongJin Kim <dge03078@gmail.com>
2020-07-17 18:53:35 +0900

    Updates

---
+++ precache-manifest.be53a32817b21e8a57157ad4ca605ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f6d8e296d24c7629114502c88ec2a1",
+    "url": "/movie_app_nomad/index.html"
+  },
+  {
+    "revision": "63f192bed3499f6e90cb",
+    "url": "/movie_app_nomad/static/css/main.4c48018f.chunk.css"
+  },
+  {
+    "revision": "6658c1e643270f2c4d66",
+    "url": "/movie_app_nomad/static/js/2.6029b64e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_nomad/static/js/2.6029b64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f192bed3499f6e90cb",
+    "url": "/movie_app_nomad/static/js/main.c9d4218b.chunk.js"
+  },
+  {
+    "revision": "589abfb683ef8f0720a3",
+    "url": "/movie_app_nomad/static/js/runtime-main.808ae562.js"
+  }
+]);
